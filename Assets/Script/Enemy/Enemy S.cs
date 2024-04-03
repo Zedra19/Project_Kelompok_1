96d5af21464e9bc837c809d4793cff523ddf7a88
Lychnus15 <nendraok@gmail.com>
2024-04-03 13:38:22 +0700

    Enemy S
    walawe mas faiz dikejar polisi cik

--- Assets/Script/Enemy/Enemy S.cs
+++ Assets/Script/Enemy/Enemy S.cs
@@ -7,10 +7,12 @@ public class EnemyS : MonoBehaviour
     public float Speed = 5.0f;
     public float Gravity = 9.81f;
     private CharacterController _characterController;
+    private Animator _animator; // Animator component
 
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>(); // Get the Animator component
     }
 
     void Update()
@@ -28,19 +30,28 @@ public class EnemyS : MonoBehaviour
             movement.y -= Gravity * Time.deltaTime;
 
             _characterController.Move(movement);
+
+            // Set animator parameters based on movement
+            if (movement.magnitude > 0)
+            {
+                _animator.SetBool("IsMoving", true); // Set IsMoving parameter to true
+            }
+            else
+            {
+                _animator.SetBool("IsMoving", false); // Set IsMoving parameter to false
+            }
         }
         else
         {
             Debug.LogWarning("Player object is not found!");
         }
     }
+    
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (hit.gameObject.CompareTag("Player"))
         {
-            
+            // You can add code here for when the enemy collides with the player
         }
     }
 }
-
-
