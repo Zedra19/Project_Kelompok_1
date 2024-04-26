265169fb53e6eaa1c6c00abbd556ed6677f50044
Zolla Humicha <zollahr27@gmail.com>
2024-04-26 17:56:28 +0700

    Implementasi Navigation AI & Fix Bug

--- Assets/Script/Enemy/PatternPatih.cs
+++ Assets/Script/Enemy/PatternPatih.cs
@@ -7,7 +7,7 @@ public class PatternPatih : MonoBehaviour
     public Transform player;
     public int damage, HP, MaxHP;
 
-    public float moveSpeed = 3f;
+    public float moveSpeed = 5f;
     public float attackRange = 7f;
     public float triggerRange = 4f;
     public float CastDuration = 3f;
@@ -47,6 +47,12 @@ public class PatternPatih : MonoBehaviour
     {
         _playerHealth = GetComponent<Health>();
         _navAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        _navAgent.speed = moveSpeed;
+        if (IsRage)
+        {
+            Renderer enemyRenderer = GetComponent<Renderer>();
+            enemyRenderer.material = rageMaterial;
+        }
     }
 
     void Update()
@@ -61,17 +67,25 @@ public class PatternPatih : MonoBehaviour
 
     public void ChasingPlayer()
     {
-        if (player != null)
-        {
-            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-            if (distanceToPlayer > triggerRange && CanMove && !IsStunned)
+        if (HP <= 0.5 * MaxHP)
             {
-                Vector3 direction = player.position - transform.position;
-                direction.y = 0f;
-                direction.Normalize();
-                Vector3 movement = direction * moveSpeed * Time.deltaTime;
-                transform.position += movement;
+                IsRage = true;
             }
+
+        if (player != null && _navAgent != null)
+        {
+            if (IsStunned && !Charging)
+            {
+                _navAgent.isStopped = true;
+                return;
+            }
+            else
+            {
+                _navAgent.isStopped = false;
+            }
+
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
             if (distanceToPlayer <= triggerRange && CanMove && !IsStunned)
             {
                 Charging = true;
@@ -82,84 +96,53 @@ public class PatternPatih : MonoBehaviour
                     Charging = false;
                 }
             }
+
+            _navAgent.SetDestination(player.position);
+            _navAgent.stoppingDistance = triggerRange;
         }
     }
 
     public void Attack()
     {
-        if ((float)HP > 0.5f * MaxHP)
+        Vector3 attackDirection = player.position - transform.position;
+        attackDirection.y = 0f;
+        attackDirection.Normalize();
+
+        // Hitung rotasi hitbox agar menghadap ke arah pemain
+        Quaternion hitboxRotation = Quaternion.LookRotation(attackDirection);
+
+        // Tentukan titik awal hitbox di bagian depan musuh (wajah musuh)
+        Vector3 startPoint = transform.position + transform.forward * 1f;
+
+        // Tentukan titik akhir hitbox yang berjarak 7 unit dari startPoint ke arah pemain
+        Vector3 endPoint = startPoint + attackDirection * 7f;
+
+        // Buat instance dari hitbox area serangan
+        hitboxInstance = Instantiate(hitboxPrefab, startPoint, hitboxRotation);
+
+        // Tentukan panjang hitbox berdasarkan jarak antara startPoint dan endPoint
+        float hitboxLength = Vector3.Distance(startPoint, endPoint);
+
+        // Atur skala hitbox agar sesuai dengan panjang yang dihitung
+        if (!IsRage)
         {
-            Vector3 attackDirection = player.position - transform.position;
-            attackDirection.y = 0f;
-            attackDirection.Normalize();
-
-            // Hitung rotasi hitbox agar menghadap ke arah pemain
-            Quaternion hitboxRotation = Quaternion.LookRotation(attackDirection);
-
-            // Tentukan titik awal hitbox di bagian depan musuh (wajah musuh)
-            Vector3 startPoint = transform.position + transform.forward * 1f;
-
-            // Tentukan titik akhir hitbox yang berjarak 7 unit dari startPoint ke arah pemain
-            Vector3 endPoint = startPoint + attackDirection * 7f;
-
-            // Buat instance dari hitbox area serangan
-            hitboxInstance = Instantiate(hitboxPrefab, startPoint, hitboxRotation);
-
-            // Tentukan panjang hitbox berdasarkan jarak antara startPoint dan endPoint
-            float hitboxLength = Vector3.Distance(startPoint, endPoint);
-
-            // Atur skala hitbox agar sesuai dengan panjang yang dihitung
             hitboxInstance.transform.localScale = new Vector3(1f, 1f, hitboxLength);
-
-            Renderer hitboxRenderer = hitboxInstance.GetComponent<Renderer>();
-            if (hitboxRenderer != null)
-            {
-                hitboxRenderer.material.color = Color.red;
-            }
-
-            CanMove = false;
-
-            StartCoroutine(StunEnemy());
-            StartCoroutine(EnableMovementAfterCast());
         }
-        
-        if ((float)HP <= 0.5f * MaxHP)
+        if (IsRage)
         {
-            Renderer enemyRenderer = GetComponent<Renderer>();
-            enemyRenderer.material = rageMaterial;
-            Vector3 attackDirection = player.position - transform.position;
-            attackDirection.y = 0f;
-            attackDirection.Normalize();
-
-            // Hitung rotasi hitbox agar menghadap ke arah pemain
-            Quaternion hitboxRotation = Quaternion.LookRotation(attackDirection);
-
-            // Tentukan titik awal hitbox di bagian depan musuh (wajah musuh)
-            Vector3 startPoint = transform.position + transform.forward * 1f; // Misalnya, jarak hitbox dimulai dari 1 unit di depan musuh
-
-            // Tentukan titik akhir hitbox yang berjarak 7 unit dari startPoint ke arah pemain
-            Vector3 endPoint = startPoint + attackDirection * 7f;
-
-            // Buat instance dari hitbox area serangan
-            hitboxInstance = Instantiate(hitboxPrefab, startPoint, hitboxRotation);
-
-            // Tentukan panjang hitbox berdasarkan jarak antara startPoint dan endPoint
-            float hitboxLength = Vector3.Distance(startPoint, endPoint);
-
-            // Atur skala hitbox agar sesuai dengan panjang yang dihitung
             hitboxInstance.transform.localScale = new Vector3(3f, 1f, hitboxLength);
+        }
+           
+        Renderer hitboxRenderer = hitboxInstance.GetComponent<Renderer>();
+        if (hitboxRenderer != null)
+        {
+            hitboxRenderer.material.color = Color.red;
+        }
 
-            Renderer hitboxRenderer = hitboxInstance.GetComponent<Renderer>();
-            if (hitboxRenderer != null)
-            {
-                hitboxRenderer.material.color = Color.red;
-            }
+        CanMove = false;
 
-            CanMove = false;
-
-            StartCoroutine(StunEnemy());
-            StartCoroutine(EnableMovementAfterCast());
-        } 
+        StartCoroutine(StunEnemy());
+        StartCoroutine(EnableMovementAfterCast());
     }
 
     IEnumerator EnableMovementAfterCast()
@@ -172,81 +155,44 @@ public class PatternPatih : MonoBehaviour
 
     IEnumerator StunEnemy()
     {
-        if ((float)HP > 0.5f * MaxHP)
+        yield return new WaitForSeconds(0.5f);
+
+        if (hitboxInstance != null)
         {
-            yield return new WaitForSeconds(0.5f);
-
-            if (hitboxInstance != null)
-            {
-                Destroy(hitboxInstance);
-            }
-
-            IsStunned = true;
-
-            Renderer enemyRenderer = GetComponent<Renderer>();
-            if (enemyRenderer != null && stunMaterial != null)
-            {
-                enemyRenderer.material = stunMaterial;
-            }
-
-            if (_playerHealth != null)
-            {
-                _playerHealth.TakeDamage(damage);
-            }
-
-            while (StunDuration > 0f)
-            {
-                yield return new WaitForSeconds(Time.deltaTime);
-                StunDuration -= Time.deltaTime;
-            }
-
-            StunDuration = 3f;
-
-            if (enemyRenderer != null && normalMaterial != null)
-            {
-                enemyRenderer.material = normalMaterial;
-            }
-
-            IsStunned = false;
-            CanMove = true;
+            Destroy(hitboxInstance);
         }
-        if((float)HP <= 0.5f * MaxHP)
+
+        IsStunned = true;
+
+        Renderer enemyRenderer = GetComponent<Renderer>();
+        if (enemyRenderer != null && stunMaterial != null)
         {
-            yield return new WaitForSeconds(0.5f);
-
-            if (hitboxInstance != null)
-            {
-                Destroy(hitboxInstance);
-            }
-
-            IsStunned = true;
-
-            Renderer enemyRenderer = GetComponent<Renderer>();
-            if (enemyRenderer != null && stunMaterial != null)
-            {
-                enemyRenderer.material = stunMaterial;
-            }
-
-            if (_playerHealth != null)
-            {
-                _playerHealth.TakeDamage(damage);
-            }
-
-            while (StunDuration > 0f)
-            {
-                yield return new WaitForSeconds(Time.deltaTime);
-                StunDuration -= Time.deltaTime;
-            }
-
-            StunDuration = 3f;
-
-            if (enemyRenderer != null && rageMaterial != null)
-            {
-                enemyRenderer.material = rageMaterial;
-            }
-
-            IsStunned = false;
-            CanMove = true;
+            enemyRenderer.material = stunMaterial;
         }
+
+        if (_playerHealth != null)
+        {
+            _playerHealth.TakeDamage(damage);
+        }
+
+        while (StunDuration > 0f)
+        {
+            yield return new WaitForSeconds(Time.deltaTime);
+            StunDuration -= Time.deltaTime;
+        }
+
+        StunDuration = 3f;
+
+        if (!IsRage)
+        {
+            enemyRenderer.material = normalMaterial;
+        }
+        if (IsRage)
+        {
+            enemyRenderer.material = rageMaterial;
+        }
+
+        IsStunned = false;
+        CanMove = true;
     }
 }
