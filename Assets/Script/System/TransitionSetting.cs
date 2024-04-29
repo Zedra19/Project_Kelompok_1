fd22f979473a62c33178f28d887d33e4891d6784
Zolla Humicha <zollahr27@gmail.com>
2024-04-29 17:28:33 +0700

    Panel Setting In Out Animation

--- Assets/Script/System/TransitionSetting.cs
+++ Assets/Script/System/TransitionSetting.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class TransitionSetting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform box;
+    public CanvasGroup BG;
+    public GameObject Menu;
+    public GameObject Setting;
+
+    private void OnEnable()
     {
-        
+        BG.alpha = 0;
+        BG.LeanAlpha(1, 0.5f);
+
+        box.localPosition = new Vector2(+Screen.width, 0);
+        box.LeanMoveLocalX(0, 0.5f).setEaseOutExpo().delay = 0.1f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Open()
     {
-        
+        BG.LeanAlpha(0, 0.5f);
+        box.LeanMoveLocalX(+Screen.width, 0.5f).setEaseInExpo().delay = 0.1f;
+    }
+
+    public void OnDisable()
+    {
+        BG.LeanAlpha(0, 0.5f);
+        box.LeanMoveLocalX(+Screen.width, 0.5f).setEaseInExpo().delay = 0.1f;
+    }
+
+    public void Close()
+    {
+        StartCoroutine(CloseAnimation());
+    }
+
+    private IEnumerator CloseAnimation()
+    {
+        BG.LeanAlpha(1, 0.5f);
+        box.LeanMoveLocalX(0, 0.5f).setEaseOutExpo().delay = 0.1f;
+
+        yield return new WaitForSeconds(0.75f);
+
+        Setting.SetActive(false);
+        Menu.SetActive(true);
     }
 }
+
