da09426916603407cd74d75545a50559f1b4980a
Zolla Humicha <zollahr27@gmail.com>
2024-05-06 16:36:27 +0700

    Revert "Merge branch 'Script' of https://github.com/Zedra19/Project_Kelompok_1 into Script"
    This reverts commit 6ec44f2ea2fd919a964ef608c4e4eecf1d2414b0, reversing
    changes made to b5a21e088ec55ff60181b04c08ad3f2a8239ce35.

--- Assets/Script/System/Score/SaveScore.cs
+++ Assets/Script/System/Score/SaveScore.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class SaveScore
+public class SaveScore : MonoBehaviour
 {
     public string Name;
     public int Score;
