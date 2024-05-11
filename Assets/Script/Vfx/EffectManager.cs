74193134dfbdb1be7209eb0e08305e84b2474674
Lychnus15 <nendraok@gmail.com>
2024-05-11 18:09:47 +0700

    Vfx slur 3==>
    Keris done, Dukun done, enemy slash done (tinggal milih cik), petani undev, enemy dissappear undev

--- Assets/Script/Vfx/EffectManager.cs
+++ Assets/Script/Vfx/EffectManager.cs
@@ -1,58 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
+namespace EffectSceneManager
+{
 public class EffectManager : MonoBehaviour
 {
-    public static EffectManager Instance;
-    public VFX[] EffectVFX;
-    
-    private void Awake()
+    public List<GameObject> effects;
+    public List<KeyCode> keys;
+    // Start is called before the first frame update
+    void Start()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    public void PlayVFXEndless(string name, Vector3 spawnPosition)
-    {
-        VFX s = Array.Find(EffectVFX, x => x.Name == name);
         
-        if(s == null)
-        {
-            Debug.Log("VFX Not Found");
-        }
-        else
-        {
-            GameObject vfxInstance = GameObject.Instantiate(s.Visual, spawnPosition, Quaternion.identity);
-        }
     }
 
-    public void PlayVFX(string name, Vector3 spawnPosition)
+    // Update is called once per frame
+    void Update()
     {
-        StartCoroutine(VFXOneShot(name, spawnPosition, 2f));
-    }
-
-    private IEnumerator VFXOneShot(string name, Vector3 spawnPosition, float delay)
-    {
-        VFX s = Array.Find(EffectVFX, x => x.Name == name);
-
-        if(s == null)
+        for (int i = 0; i < keys.Count; i++)
         {
-            Debug.Log("VFX Not Found");
-        }
-        else
-        {
-            GameObject vfxInstance = GameObject.Instantiate(s.Visual, spawnPosition, Quaternion.identity);
-            yield return new WaitForSeconds(delay);
-            Destroy(vfxInstance);
+            if (Input.GetKeyDown(keys[i]))
+            {
+                Instantiate<GameObject>(effects[i]);
+            }
         }
     }
+}
 }
