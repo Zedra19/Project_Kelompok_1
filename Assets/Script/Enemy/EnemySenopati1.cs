32208090310796f886fcf0d6af9554b094da35f4
ZeaH46 <lufiilhamzahulhaq@gmail.com>
2024-05-11 14:45:55 +0700

    Senopati + NavMesh

--- Assets/Script/Enemy/EnemySenopati1.cs
+++ Assets/Script/Enemy/EnemySenopati1.cs
@@ -7,28 +7,24 @@ public class Senopati : MonoBehaviour
     public Transform player;
     public Transform[] hitboxSpawnPoints;
     public GameObject hitboxPrefab;
+    public GameObject areaAttackHitbox;
     public GameObject rageHitboxPrefab;
     public Transform attackSpawnPoint;
     public BossHealth bossHealth;
 
-    public float detectionRange = 10000000000f;
     public float attackRange = 6f;
-    public float retreatRange = 3f;
-    public float chaseSpeed = 5f;
+    // public float retreatRange = 3f;
     public float hitboxSpeed = 10f;
     public float hitboxDuration = 1f;
-    public float rageDuration = 20f;
     public float attackCooldown = 4f;
-    public float HP;
     public bool isGettingHitInThisHit;
+    public float HP;
 
     private bool isRageMode = false;
     private bool canAttack = true;
-    private Transform _playerTransform;
     private NavMeshAgent _navAgent;
     private float currentHealth;
     private float stageDuration = 0f;
-
 
     void Start()
     {
@@ -38,7 +34,6 @@ public class Senopati : MonoBehaviour
 
     void Update()
     {
-        HP = currentHealth;
         stageDuration += Time.deltaTime;
         if (player == null)
         {
@@ -48,64 +43,61 @@ public class Senopati : MonoBehaviour
 
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
-        if (distanceToPlayer <= detectionRange)
+        if (distanceToPlayer <= attackRange)
         {
-            if (distanceToPlayer <= attackRange && canAttack)
-            {
-                if (!isRageMode)
-                    Attack();
-                else
-                    RageAttack();
-            }
-            else
-            {
-                if (distanceToPlayer <= retreatRange)
-                {
-                    Retreat();
-                }
-                else
-                {
-                    ChasePlayer();
-                }
-            }
+            _navAgent.isStopped = true; 
+
+            if (!isRageMode && canAttack)
+                Attack();
+            else if (isRageMode && canAttack)
+                RageAttack();
+        }
+        // else if (distanceToPlayer <= retreatRange)
+        // {
+        //     Retreat();
+        // }
+        else
+        {
+            _navAgent.isStopped = false; 
+            ChasePlayer();
         }
 
         if (stageDuration >= 20f && !isRageMode)
         {
             EnterRageMode();
         }
+
+        if (isRageMode && !canAttack)
+        {
+            _navAgent.isStopped = true;
+        }
     }
 
     private void ChasePlayer()
     {
-        Vector3 moveDirection = (player.position - transform.position).normalized;
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-
-        if (distanceToPlayer > attackRange)
-        {
-            // _navAgent.SetDestination(player.transform.position);
-            transform.position += moveDirection * chaseSpeed * Time.deltaTime;
-            transform.LookAt(player);
-        }
+        _navAgent.SetDestination(player.position);
     }
 
-    private void Retreat()
-    {
-        Vector3 retreatDirection = (transform.position - player.position).normalized;
-        retreatDirection.y = 0f;
-        float retreatSpeed = chaseSpeed * 0.2f;
-        Vector3 retreatPosition = transform.position + retreatDirection * retreatSpeed * Time.deltaTime;
-        if (!Physics.Raycast(transform.position, retreatDirection, 1f))
-        {
-            transform.position = retreatPosition;
-        }
-        transform.LookAt(player);
-    }
+    // private void Retreat()
+    // {
+    //     Vector3 retreatDirection = (transform.position - player.position).normalized;
+    //     retreatDirection.y = 0f;
+    //     float retreatSpeed = _navAgent.speed * 0.2f;
+    //     Vector3 retreatPosition = transform.position + retreatDirection * retreatSpeed * Time.deltaTime;
+    //     if (!Physics.Raycast(transform.position, retreatDirection, 1f))
+    //     {
+    //         transform.position = retreatPosition;
+    //     }
+    //     transform.LookAt(player);
+    // }
 
     private void Attack()
     {
         Vector3 directionToPlayer = (player.position - transform.position).normalized;
         SpawnHitbox(directionToPlayer);
+        Vector3 spawnPosition = attackSpawnPoint.position;
+        GameObject areaHitbox = Instantiate(areaAttackHitbox, spawnPosition, Quaternion.identity);
+        Destroy(areaHitbox, hitboxDuration);
         StartCoroutine(AttackCooldown());
     }
 
@@ -128,7 +120,7 @@ public class Senopati : MonoBehaviour
             }
             else
             {
-                Debug.LogError("Spawn points is null.");
+                Debug.LogError("Spawn point is null.");
             }
         }
     }
@@ -139,7 +131,6 @@ public class Senopati : MonoBehaviour
         GameObject rageHitbox = Instantiate(rageHitboxPrefab, spawnPosition, Quaternion.identity);
         rageHitbox.GetComponent<Rigidbody>().velocity = Vector3.zero;
         Destroy(rageHitbox, hitboxDuration);
-        Destroy(rageHitbox, 1f);
     }
 
     private IEnumerator AttackCooldown()
@@ -153,14 +144,6 @@ public class Senopati : MonoBehaviour
     {
         Debug.Log("Boss enters rage mode!");
         isRageMode = true;
-        Invoke("ExitRageMode", rageDuration);
-    }
-
-    private void ExitRageMode()
-    {
-        Debug.Log("Boss exits rage mode!");
-        isRageMode = false;
-        ResetStageDuration();
     }
 
     public float GetCurrentHealth()
@@ -173,13 +156,8 @@ public class Senopati : MonoBehaviour
         currentHealth -= damage;
         if (currentHealth <= bossHealth.BossMaxHealth * 0.5f && !isRageMode)
         {
-            EnterRageMode(); 
+            EnterRageMode();
         }
-    }
-
-    private void ResetStageDuration()
-    {
-        stageDuration = 0f;
     }
 
     private void OnEnable()
--- Assets/Script/Enemy/HitBoxSenopati.cs
+++ Assets/Script/Enemy/HitBoxSenopati.cs
@@ -4,21 +4,13 @@ public class Hitbox : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        // Periksa jika objek yang masuk ke dalam hitbox adalah pemain
         if (other.CompareTag("Player"))
         {
-            // Dapatkan komponen Health dari pemain
             Health playerHealth = other.GetComponent<Health>();
-            
-            // Periksa apakah komponen Health diperoleh dengan benar
             if (playerHealth != null)
             {
                 Debug.Log("Player Hit");
-                
-                // Sebelum mengakses metode TakeDamage, pastikan metode tersebut didefinisikan di komponen Health
                 playerHealth.TakeDamage(1);
-                
-                // Hancurkan objek hitbox setelah menyentuh pemain
                 Destroy(gameObject);
             }
         }
