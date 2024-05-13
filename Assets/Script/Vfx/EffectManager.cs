bfe90d4cd0d4bf72b0d91bfa972b20ecf685f378
Zolla Humicha <zollahr27@gmail.com>
2024-05-13 09:36:58 +0700

    Update VFX Manager, Fix Error

--- Assets/Script/Vfx/EffectManager.cs
+++ Assets/Script/Vfx/EffectManager.cs
@@ -1,29 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-namespace EffectSceneManager
-{
 public class EffectManager : MonoBehaviour
 {
-    public List<GameObject> effects;
-    public List<KeyCode> keys;
-    // Start is called before the first frame update
+    public static EffectManager Instance;
+    public VFX[] EffectVFX;
+    public GameObject SourceVFX;
+
+    private void Awake()
+    {
+
+    }
+
     void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayVFXEndless(string name, Vector3 spawnPosition)
     {
-        for (int i = 0; i < keys.Count; i++)
+        VFX s = Array.Find(EffectVFX, x => x.Name == name);
+
+        if(s == null)
         {
-            if (Input.GetKeyDown(keys[i]))
-            {
-                Instantiate<GameObject>(effects[i]);
-            }
+            Debug.Log("VFX Not Found");
+        }
+        else
+        {
+            GameObject vfxInstance = GameObject.Instantiate(s.Visual, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    public void PlayVFX(string name, Vector3 spawnPosition)
+    {
+        StartCoroutine(VFXOneShot(name, spawnPosition, 2f));
+    }
+
+    private IEnumerator VFXOneShot(string name, Vector3 spawnPosition, float delay)
+    {
+        VFX s = Array.Find(EffectVFX, x => x.Name == name);
+
+        if(s == null)
+        {
+            Debug.Log("VFX Not Found");
+        }
+        else
+        {
+            GameObject vfxInstance = GameObject.Instantiate(s.Visual, spawnPosition, Quaternion.identity);
+            yield return new WaitForSeconds(delay);
+            Destroy(vfxInstance);
         }
     }
 }
-}
+// }
