de711d1b8f1ef47b3d1127dd63b8f9131a470537
Zedra19 <zedra1911@gmail.com>
2024-05-15 12:06:05 +0700

    Profile Menu (sementara)
    Bisa add dan Select profile

--- Assets/Script/System/Profile/ProfileMenu.cs
+++ Assets/Script/System/Profile/ProfileMenu.cs
@@ -9,54 +9,93 @@ public class ProfileMenu : MonoBehaviour
     public GameObject inputName;
     public GameObject inputField;
     public GameObject selectProfile;
+    public Text profile1;
+    public Text profile2;
+    public Text profile3;
     // Start is called before the first frame update
     void Start()
     {
-        if(Profile.CurrentPlayerIndex == 0)
+        kondisi_profile();
+    }
+
+    public void kondisi_profile()
+    {
+        if(string.IsNullOrEmpty(PlayerPrefs.GetString("Profile1")) && string.IsNullOrEmpty(PlayerPrefs.GetString("Profile2")) && string.IsNullOrEmpty(PlayerPrefs.GetString("Profile3")))
         {
+            profile.SetActive(true);
             inputName.SetActive(true);
         }else
         {
+            profile.SetActive(true);
             selectProfile.SetActive(true);
         }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
+        profile1.text = PlayerPrefs.GetString("Profile1");
+        profile2.text = PlayerPrefs.GetString("Profile2");
+        profile3.text = PlayerPrefs.GetString("Profile3");
     }
 
     public void SaveName()
     {
         var name = inputField.GetComponent<InputField>().text;
-        Debug.Log(PlayerPrefs.GetString("Profile1").ToString());
-        if(PlayerPrefs.GetString("Profile1") == null){
+        if(string.IsNullOrEmpty(PlayerPrefs.GetString("Profile1"))){
             PlayerPrefs.SetString("Profile1", name);
-            if(Profile.CurrentPlayerIndex == 0){
-                Profile.CurrentPlayerIndex = 1;
-            }
-            profile.SetActive(false);
-            
-        }else if(PlayerPrefs.GetString("Profile1") != null && PlayerPrefs.GetString("Profile2") == null){
+            Profile.CurrentPlayerIndex = 1;
+        }else if(!string.IsNullOrEmpty(PlayerPrefs.GetString("Profile1")) && string.IsNullOrEmpty(PlayerPrefs.GetString("Profile2"))){
             PlayerPrefs.SetString("Profile2", name);
-            if(Profile.CurrentPlayerIndex == 0){
-                Profile.CurrentPlayerIndex = 2;
-                
-            }
-            profile.SetActive(false);
-        }else if(PlayerPrefs.GetString("Profile2") != null && PlayerPrefs.GetString("Profile3") == null){
+            Profile.CurrentPlayerIndex = 2;
+        }else if(!string.IsNullOrEmpty(PlayerPrefs.GetString("Profile2")) && string.IsNullOrEmpty(PlayerPrefs.GetString("Profile3"))){
             PlayerPrefs.SetString("Profile3", name);
-            if(Profile.CurrentPlayerIndex == 0){
-                Profile.CurrentPlayerIndex = 3;
-                
-            }
+            Profile.CurrentPlayerIndex = 3; 
+        }else{
+            Debug.Log("Profile is full");
+        }
+        inputField.GetComponent<InputField>().text = "";
+        inputName.SetActive(false);
+        profile.SetActive(false);
+    }
+
+    public void profile1select()
+    {
+        Profile.CurrentPlayerIndex = 1;
+        if(string.IsNullOrEmpty(PlayerPrefs.GetString("Profile1"))){
+            selectProfile.SetActive(false);
+            inputName.SetActive(true);
+        }else{
+            selectProfile.SetActive(false);
             profile.SetActive(false);
         }
     }
 
-    public void SelectProfile()
+    public void profile2select()
     {
-        
+        Profile.CurrentPlayerIndex = 2;
+        if(string.IsNullOrEmpty(PlayerPrefs.GetString("Profile2"))){
+            selectProfile.SetActive(false);
+            inputName.SetActive(true);
+        }else{
+            selectProfile.SetActive(false);
+            profile.SetActive(false);
+        }
+    }
+
+    public void profile3select()
+    {
+        Profile.CurrentPlayerIndex = 3;
+        if(string.IsNullOrEmpty(PlayerPrefs.GetString("Profile3"))){
+            selectProfile.SetActive(false);
+            inputName.SetActive(true);
+        }else{
+            selectProfile.SetActive(false);
+            profile.SetActive(false);
+        }
+    }
+
+    public void ConfirmProfile()
+    {
+        selectProfile.SetActive(false);
     }
 }
