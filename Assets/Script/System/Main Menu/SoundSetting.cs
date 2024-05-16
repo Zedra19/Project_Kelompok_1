bfd153b3b92f370ee8fc7f0f01990f63e64eeb45
Zedra19 <zedra1911@gmail.com>
2024-05-17 04:10:41 +0700

    Fix Slider Menu
    Benerin bug, kalau balik Home, bakal reset lagi suaranya

--- Assets/Script/System/Main Menu/SoundSetting.cs
+++ Assets/Script/System/Main Menu/SoundSetting.cs
@@ -12,18 +12,15 @@ public class SoundSetting : MonoBehaviour
     }
     private IEnumerator ProcessAwake()
     {
-        if (MusicSlider != null || SFXSlider != null)
-        {
-            MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
-            SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.5f);
-        }
-
+        MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.5f);
         yield return new WaitForSeconds(0.3f);
-
-        MusicVolume();
-        SFXVolume();
     }
 
+    void Update(){
+        AudioManager.Instance.MusicSource.volume = PlayerPrefs.GetFloat("MusicVolume");
+        AudioManager.Instance.SFXSource.volume = PlayerPrefs.GetFloat("SFXVolume");
+    }
 
     public void UISFX()
     {
@@ -42,28 +39,12 @@ public class SoundSetting : MonoBehaviour
 
     public void MusicVolume()
     {
-        if (MusicSlider != null || SFXSlider != null)
-        {
-            AudioManager.Instance.MusicVolume(MusicSlider.value);
-            PlayerPrefs.SetFloat("MusicVolume", MusicSlider.value);
-        }
-        else
-        {
-            AudioManager.Instance.MusicVolume(PlayerPrefs.GetFloat("MusicVolume", 0.5f));
-        }
+        PlayerPrefs.SetFloat("MusicVolume", MusicSlider.value);
     }
 
     public void SFXVolume()
     {
-        if (MusicSlider != null || SFXSlider != null)
-        {
-            AudioManager.Instance.SFXVolume(SFXSlider.value);
-            PlayerPrefs.SetFloat("SFXVolume", SFXSlider.value);
-        }
-        else
-        {
-            AudioManager.Instance.MusicVolume(PlayerPrefs.GetFloat("SFXVolume", 0.5f));
-        }
+        PlayerPrefs.SetFloat("SFXVolume", SFXSlider.value);
     }
 
 }
