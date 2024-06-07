5ea6e34dc2212a70969290d4c980758d0c6a9e15
ZeaH46 <lufiilhamzahulhaq@gmail.com>
2024-06-07 15:26:19 +0700

    Rapiin Env + Script Enemy

--- Assets/Script/System/KillCount.cs
+++ Assets/Script/System/KillCount.cs
@@ -6,10 +6,7 @@ using UnityEngine.UI;
 public class KillCount : MonoBehaviour
 {
     public int killCount = 0;
-    public int killCountForBossSpawn = 5;
 
-    [SerializeField] private Slider bossHealthSlider; // Reference to the UI Slider
-    [SerializeField] private Text bossName; // Reference to the Boss Name
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +16,6 @@ public class KillCount : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //activate boss health here
-        /*
-        if (killCount >= killCountForBossSpawn)
-        {
-            bossHealthSlider.gameObject.SetActive(true);
-            bossName.gameObject.SetActive(true);
-        }
-        */
+
     }
 }
